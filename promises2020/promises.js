75de71987269d7e4b059f0785302b424fa772196
Jim Thorne <crazyhorse697@gmail.com>
2020-03-12 20:52:57 -0600

    first commit for readme modified promises

--- promises2020/promises.js
+++ promises2020/promises.js
@@ -1,32 +1,33 @@
 const backend = (number) => {
-    const numberTestPromise = new Promise ((resolve,reject) => {
-        if (number %2 === 0){ 
-            resolve ('This number is even')    
-        } elseif (number %2===1)
-        {reject ('The number is odd')
+	const numberTestPromise = new Promise((resolve, reject) => {
+		if (number % 2 === 0) {
+			resolve('This number is even')
+		}
+		else if(number % 2 === 1)
+		{
+			reject('The number is odd')
+		}
+	});
+	return numberTestPromise;
+};
 
-
-        }
-        
-
-    })
-    return numberTestPromise
-  
+const builtinFuncs = () => {
+	const builtInFuncsPromise = backend(1);
+	builtInFuncsPromise
+		.then((successObject) => {
+			console.log(successObject)
+		})
+        .catch((failureObject) => {console.log(failureObject)
+        })
 }
+//builtinFuncs()
 
-const builtinFuncs = (builtInFuncsPromise) => {
-        const builtInFuncsPromise = backend(1)
-        builtInFuncsPromise.then((successObject)=> {
-            console.log(successObject)
-        }) .catch ((failureObject)) =>
+const asyncAwait = async () => {
+	try {
+        const successObject = await backend(2);
+        console.log(successObject)
+	} catch (failureObject) {
+        console.log(failureObject)
     }
 }
-
-const asyncAwait = async() => {try {
-    const asyncAwaitPromise =backend(1)
-    } catch(failureObject) {
-    
-    }   
-}
-  
-
+asyncAwait()
---
+++ promises2020/readme.md
@@ -0,0 +1,12 @@
+1.What is a Promise? 
+    The promise is object that represents the eventual completion (resolve) or failure (reject) of an asynchonous operation and the resulting value
+
+2. How do we use Promises?
+    To allow the API or UI program to execute while information is being obtained for the Database server.That is one of many things that can be done when you're able to execute aynchronous logic
+
+3. What are the benefits of using Promises? 
+    A. Better definition and flow of aynchrounous logic (I assume when compared to a callback)
+    B. Can write asynchronous code in a manner closer to synchonous approach thereby reducing the possibility for errors.
+
+4. Improved readability. The callback equivalent to accomplish the same operation appears to take many more lines of code.
+
